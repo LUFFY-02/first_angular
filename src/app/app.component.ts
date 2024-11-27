db77d2b160643503085c2bed8b324b0bc37d4fcf
Luffy <singhrajvardhan69@gmail.com>
2024-11-27 19:24:52 +0530

    Implemented routing and added todos panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
   <app-header/>
-  <main>
-    <app-home/>
+  <main><router-outlet/>
   </main>
   `,
   styles: [`
